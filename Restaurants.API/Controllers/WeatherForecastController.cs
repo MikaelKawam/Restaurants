22a40e544ea66eced8153e851516d83602619e2b
Mikael <mikaelkawam.dev@gmail.com>
2024-05-15 17:48:10 -0300

    Add project files.

---
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWeatherForecastService _weatherForecastService;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,
+        IWeatherForecastService weatherForecastService)
+    {
+        _logger = logger;
+        _weatherForecastService = weatherForecastService;
+    }
+
+    [HttpGet]
+    [Route("example")]
+    public IEnumerable<WeatherForecast> Get() => _weatherForecastService.Get();
+}
