193fce2fa19be92833c09f261572ecac5a0b8d67
Mikael <mikaelkawam.dev@gmail.com>
2024-06-02 22:29:16 -0300

    feat: finish testing

---
+++ src/Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Application.Common;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Persistence;
+using System.Linq.Expressions;
+
+namespace Restaurants.Infrastructure.Repositories
+{
+    internal class RestaurantsRepository(RestaurantsDbContext dbContext)
+        : IRestaurantsRepository
+    {
+        public async Task<int> Create(Restaurant entity)
+        {
+            dbContext.Restaurants.Add(entity);
+            await dbContext.SaveChangesAsync();
+            return entity.Id;
+        }
+
+        public async Task Delete(Restaurant entity)
+        {
+            dbContext.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Restaurant entity)
+        {
+            dbContext.Restaurants.Update(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Restaurant>> GetAllAsync()
+        {
+            var restaurants = await dbContext.Restaurants.ToListAsync();
+            return restaurants;
+        }
+
+        public async Task<(IEnumerable<Restaurant>, int)> GetAllMatchingAsync(string? searchPhrase,
+            int pageSize,
+            int pageNumber,
+            string? sortBy,
+            SortDirection sortDirection
+            )
+        {
+            var searchPharseLower = searchPhrase?.ToLower();
+
+            // If search phrase is null, return all restaurants with null;
+            var baseQuery = dbContext
+                .Restaurants
+                .Where(r => searchPharseLower == null || (r.Name.ToLower().Contains(searchPharseLower)
+                         || r.Description.ToLower().Contains(searchPharseLower)));
+
+            var totalCount = await baseQuery.CountAsync();
+
+            if(sortBy != null)
+            {
+                var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
+                {
+                    {nameof(Restaurant.Name), r => r.Name},
+                    {nameof(Restaurant.Description), r => r.Description},
+                    {nameof(Restaurant.Category), r => r.Category},
+                };  
+
+                var selectedColumn = columnsSelector[sortBy];
+
+                baseQuery = sortDirection == SortDirection.Ascending
+                    ? baseQuery.OrderBy(selectedColumn)
+                    : baseQuery.OrderByDescending(selectedColumn);
+
+            }
+
+            var restaurants = await baseQuery
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (restaurants, totalCount);
+        }
+
+
+        public async Task<Restaurant?> GetByIdAsync(int id)
+        {
+            var restaurant = await dbContext.Restaurants
+               .Include(r => r.Dishes)
+               .FirstOrDefaultAsync(x => x.Id == id)
+               ;
+
+            return restaurant;
+        }
+
+        public Task SaveChanges()
+         => dbContext.SaveChangesAsync();
+    }
+}
+ 
